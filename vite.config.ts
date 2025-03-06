42a5d807055a8d85d1f800bc02c845c8dee4097e
elrufo21 <neburtnt@gmail.com>
2025-03-06 14:40:45 -0500

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/corporacionvaldeon.github.io/", // Asegura que coincida con el nombre de tu repo
 });
