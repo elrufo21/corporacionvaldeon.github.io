d09a09fbfd72368588af2033bbf108f49539b6c3
Ruben <desarrollo.linksoft2@hotmail.com>
2025-03-06 11:02:44 -0500

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/corporacionvaldeon.github.io/",
+  base: "/corporacionvaldeon.github.io/", // Asegura que coincida con el nombre de tu repo
 });
